00d49e704ced17ce9ac06acdb46436dbcf79af19
Dawn Trail <dtrail76@gmail.com>
2024-03-26 19:14:08 +0000

    set up initial fetch from umbraco

---
+++ lib/umbraco-heartcore.ts
@@ -0,0 +1,48 @@
+// Fetch content by page
+type Header = {
+    'Content-Type': string,
+    'Api-Key': string,
+    'Umb-Project-Alias': string
+}
+
+async function fetchAPI(query: string, { variables}:any = {}) {
+    const res = await fetch("https://graphql.umbraco.io", {
+        method: "POST",
+        headers:{
+                'Content-Type': 'application/json',
+                'Api-Key': '8TzZfqrJH1Yhz9PRlCUi',
+                'Umb-Project-Alias': process.env.UMBRACO_PROJECT_ALIAS,
+        } as Header,
+        body: JSON.stringify({
+            query,
+            variables
+        }),
+    });
+    const json = await res.json();
+
+    if (json.errors) {
+        console.error(json.errors);
+        throw new Error("Failed to fetch API");
+    }
+    return json.data;
+}
+
+export async function getHomePage() {
+    const data = await fetchAPI(`
+{
+    content(url: "/homepage/") {
+        name
+        ... on Home {
+        title
+        heroText
+        bodyCopy
+        exploreCopy
+        image{
+        url
+        }
+        }
+    }
+}
+`);
+    return data.content;
+}
