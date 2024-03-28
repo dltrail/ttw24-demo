45e440e45e09119a7cb6599333aac13e98c4d1c6
Dawn Trail <dtrail76@gmail.com>
2024-03-28 14:35:54 +0000

    padd out column component

--- components/TwoColumnBlock/TwoColumnBlock2.tsx
+++ components/TwoColumnBlock/TwoColumnBlock2.tsx
@@ -1,22 +1,23 @@
 import { ReactElement } from "react"
 import cn from "classnames";
-import styles from "./TwoColumnBlock.module.scss";
+import styles from "./styles/TwoColumnBlock.module.scss";
 
 type Content = {
   __typename: string
   col1: string
   col2: string
-  }
-
-type TwoColumnBlock2Props = {
-   content: Content
 }
 
-export function TwoColumnBlock2({content}:TwoColumnBlock2Props) {
-const {__typename, col1, col2} = content
+type TwoColumnBlock2Props = {
+  content: Content
+  className?: string | undefined
+}
+
+export function TwoColumnBlock2({ content, className }: TwoColumnBlock2Props) {
+  const { __typename, col1, col2 } = content
   return (
-    <div>
-    <p>This is a {__typename}</p>
+    <div className={cn(`${className ? className : ""}`, `${styles.twoColumnBlock}`)}>
+      <p>This is a {__typename}</p>
       <p>{col1}</p>
       <p>{col2}</p>
     </div>
