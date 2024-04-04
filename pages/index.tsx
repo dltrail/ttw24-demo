df166182e749827e7d623989401b019a453035d2
Dawn Trail <dtrail76@gmail.com>
2024-04-04 08:36:17 +0100

    implement explore tin component form live site

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Layout from "@/components/Layout/layout";
 import { getHomePage } from "../lib/umbraco-heartcore";
 import Container from "@/components/Container/container";
 import { TwoColumnBlock2 } from "@/components/TwoColumnBlock/TwoColumnBlock2";
+import ExploreTinnovation from "@/components/FromCurrent/ExploreTinnovation/ExploreTinnovation";
 
 export default function Home({ homepage }: any) {
   const { title, heroText, bodyCopy, exploreCopy, image, blocks } = homepage;
@@ -38,6 +39,7 @@ export default function Home({ homepage }: any) {
             </div>
           </section>
           <section className={styles.section} dangerouslySetInnerHTML={{ __html: exploreCopy }}/>
+          <ExploreTinnovation/>
         </Container>
       </Layout>
     </>
