d9cec7e1060b92f41417d0fdcf40c51e9e8d1586
Dawn Trail <dtrail76@gmail.com>
2024-04-05 06:33:56 +0100

    update post preview

--- components/PostPreview/PostPreview.tsx
+++ components/PostPreview/PostPreview.tsx
@@ -2,16 +2,21 @@ import { Post } from "@/types"
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./postPreview.module.scss";
+import moment from "moment";
+import { longDateFormat } from "@/lib/constants";
 
 type PostPreviewProps = {
     post: Post
 }
 
 export default function PostPreview({ post }: PostPreviewProps) {
-    const { coverImage, author, slug } = post
+    const { coverImage, summaryTitle, date, slug, tags, readTime } = post
+
     return (
         <div className={styles.post} >
-            <Image
+        
+            <Link href={slug}>
+                <Image
                 src={coverImage.url}
                 alt="hero image"
                 quality={60}
@@ -24,10 +29,13 @@ export default function PostPreview({ post }: PostPreviewProps) {
                 width={100}
                 height={100}
             />
-            <p>{post.title}</p>
-
-            <p>{author.name}</p>
-            <Link href={slug}>Read article</Link>
+            <p className={styles.title}>{summaryTitle}</p>
+            
+            </Link>
+            <div className={styles.previewMeta}>
+            <p>{tags.map((tag, i)=>(<span key={i}>{tag}</span>))}</p>
+                <p className={styles.date}>{moment(date).utc().format(longDateFormat)}</p>
+            </div>
         </div>
     )
 }
