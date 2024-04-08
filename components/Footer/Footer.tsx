04643bfb9e38f16ffaed847ecc005d2892e14746
Dawn Trail <dtrail76@gmail.com>
2024-04-08 11:34:40 +0100

    simple footer stuff

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,9 +1,18 @@
+import Logo from "../Logo/logo";
+import styles from "./Footer.module.scss"
+
 export default function Footer() {
     return (
-        <footer>
-            <link rel="icon" href="/favicon.ico" />
-            <div className="">
-                <p>Footer content</p>
+        <footer className={styles.footer}>
+            <p className={styles.header}>Going strong since 2001.</p>
+            <div className={styles.footerMain}>
+                <div><p>Address</p></div>
+                <div className={styles.footerNav}>
+                <div><p>About us</p></div>
+                <div><p>Work</p></div>
+                <div><p>Tinnovation</p></div>
+                </div>
+                <div><p>Footer content</p></div>
             </div>
         </footer>
     );
